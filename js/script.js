78001a25408d7df1688156d4e336b458b46d95ef
Swati Sahay <swatiranjan0111@gmail.com>
2018-05-30 23:20:45 -0700

    added other functionality of calculator like multiply subtraction and division

--- js/script.js
+++ js/script.js
@@ -23,10 +23,46 @@ var remainder= function(number1, number2) {
 $(document).ready(function() {
   $("form#add").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+
     var result = add(number1, number2);
+
     $("#output").text(result);
+  });
+
+  $("form#mul").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+
+    var result1 = mul(number1, number2);
+
+    $("#output").text(result1);
+
+
+  });
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+
+    var result1 = sub(number1, number2);
+
+    $("#output").text(result1);
+
+
+  });
+  $("form#div").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#num1").val());
+    var number2 = parseInt($("#num2").val());
+
+    var result1 = remainder(number1, number2);
+
+    $("#output").text(result1);
+
+
   });
 
 });
